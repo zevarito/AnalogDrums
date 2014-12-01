2be667fd0e307265349086633638714d8349d586
Alvaro Gil <zevarito@gmail.com>
2014-12-01 01:33:44 -0200

    something stable?

--- AnalogDrums.ino
+++ AnalogDrums.ino
@@ -1,3 +1,15 @@
+/*
+ * BOF preprocessor bug prevent
+ * insert me on top of your arduino-code
+ */
+#define nop() __asm volatile ("nop")
+#if 1
+nop();
+#endif
+/*
+ * EOF preprocessor bug prevent
+*/
+
 #define DEBUG 0
 
 #if DEBUG
@@ -11,16 +23,16 @@ long loops = 0;
 /* 
  * Configurable stuff
  */
-
+ 
 // Threshold value to read signal as a valid stroke.
-#define INPUT_SIGNAL_MINIMUN_STROKE 10
+#define INPUT_SIGNAL_MINIMUN_STROKE 20
 
 // Silent time in millisecons an instrument should wait before.
 // be read his input again.
-#define PLAYED_AGAIN_WAITTIME_MILLISECS 24
+#define PLAYED_AGAIN_WAITTIME_MILLISECS 16
 
 // Analog reads amount needed to define the best stroke.
-#define RESOLUTION_PASSES 1
+#define RESOLUTION_PASSES 32
 
 // Signal (1024 max) will be divided for this number and the
 // result will be constrain to 127 max as it is the maximun
@@ -38,22 +50,24 @@ struct Pad {
   bool isSeekingBestStroke;
   float velocityMultiplier;
   int waitTimeMillis;
+  bool isPlaying;
 };
 
 // Function headers
 int readInputInstrument(int analogPort, Pad *pInstrument);
 void resetInstrument(Pad *pInstrument);
 void playInstrument(Pad *pInstrument, int velocity);
+void readInputAndPlay(int index);
 
 // Input / Instruments mapping
 Pad instruments[7] = {
-  {144, 128, 38, 0, 0, 0, false, 1.8, PLAYED_AGAIN_WAITTIME_MILLISECS}, // Snare
-  {145, 129, 51, 0, 0, 0, false, 1.8, PLAYED_AGAIN_WAITTIME_MILLISECS}, // Ride
-  {146, 130, 35, 0, 0, 0, false, 1.8, PLAYED_AGAIN_WAITTIME_MILLISECS}, // Bass Drum
-  {147, 131, 42, 0, 0, 0, false, 1.8, PLAYED_AGAIN_WAITTIME_MILLISECS}, // Closed Hi Hat
-  {148, 132, 50, 0, 0, 0, false, 1.8, PLAYED_AGAIN_WAITTIME_MILLISECS}, // High Tom
-  {149, 133, 41, 0, 0, 0, false, 1.8, PLAYED_AGAIN_WAITTIME_MILLISECS}, // Low Floor Tom
-  {150, 134, 49, 0, 0, 0, false, 1.8, PLAYED_AGAIN_WAITTIME_MILLISECS}  // Crash
+  {144, 128, 38, 0, 0, 0, false, 1.8, PLAYED_AGAIN_WAITTIME_MILLISECS, false}, // Snare
+  {145, 129, 51, 0, 0, 0, false, 1.8, PLAYED_AGAIN_WAITTIME_MILLISECS, false}, // Ride
+  {146, 130, 35, 0, 0, 0, false, 1.6, PLAYED_AGAIN_WAITTIME_MILLISECS, false}, // Bass Drum
+  {147, 131, 42, 0, 0, 0, false, 1.8, PLAYED_AGAIN_WAITTIME_MILLISECS, false}, // Closed Hi Hat
+  {148, 132, 50, 0, 0, 0, false, 1.8, PLAYED_AGAIN_WAITTIME_MILLISECS, false}, // High Tom
+  {149, 133, 41, 0, 0, 0, false, 1.8, PLAYED_AGAIN_WAITTIME_MILLISECS, false}, // Low Floor Tom
+  {150, 134, 49, 0, 0, 0, false, 1.8, PLAYED_AGAIN_WAITTIME_MILLISECS, false}  // Crash
 };
 
 #define SNARE  0
@@ -78,11 +92,11 @@ void setup() {
 
 byte INPUT_INSTRUMENT_READING_LOOP[6][2] = {
   {0, BDRUM},
-  {1, CHIHAT},
+  {1, CRASH},
   {2, SNARE},
   {3, RIDE},
   {4, SNARE},
-  {5, RIDE},
+  {5, CHIHAT},
 };
 
 int currentInputInstrumentIndex = 0;
@@ -94,20 +108,21 @@ void loop() {
     loops++;
   #endif
   
-  int reading;
-  Pad *pInstrument;
-  
+  // Method: all in a loop
+  /*
   currentInputInstrumentIndex = 0;
-  
-  while(currentInputInstrumentIndex<6) {
-    pInstrument = &instruments[INPUT_INSTRUMENT_READING_LOOP[currentInputInstrumentIndex][1]];
-    reading = readInputInstrument(INPUT_INSTRUMENT_READING_LOOP[currentInputInstrumentIndex][0], pInstrument);
-    if (reading > 0)
-      playInstrument(pInstrument, constrain(reading / INPUT_SIGNAL_DIVISOR, 0, 127));
-    
-     currentInputInstrumentIndex++;
-    
+  while(currentInputInstrumentIndex < 6) {
+    readInputAndPlay(currentInputInstrumentIndex);
+    currentInputInstrumentIndex++;
   }
+  */
+ 
+  // Method: One in a loop
+  readInputAndPlay(currentInputInstrumentIndex);
+  if (currentInputInstrumentIndex == 5)
+    currentInputInstrumentIndex = 0;
+  else
+    currentInputInstrumentIndex++;
   
   #if DEBUG
     if(currentMillis - lastMillis > 1000){
@@ -118,6 +133,22 @@ void loop() {
       loops = 0;
     }
   #endif
+}
+
+void readInputAndPlay(int index) {
+  int reading;
+  Pad *pInstrument;
+  
+  pInstrument = &instruments[INPUT_INSTRUMENT_READING_LOOP[index][1]];
+  
+  reading = readInputInstrument(INPUT_INSTRUMENT_READING_LOOP[index][0], pInstrument);
+  if (reading > 0) {
+    if (pInstrument->isPlaying) {
+      midiMsg(pInstrument->noteOn, pInstrument->note, 0);
+      pInstrument->isPlaying = false;
+    }
+    playInstrument(pInstrument, constrain(reading / INPUT_SIGNAL_DIVISOR, 40, 127));
+  }
 }
 
 // Reads analog port and determines if it is a valid keystroke or not.
@@ -166,9 +197,10 @@ void resetInstrument(Pad *pInstrument) {
 void playInstrument(Pad *pInstrument, int velocity) {
   turnLedOn();
   
-  // Send MIDI output message, first off then on.
-  midiMsg(pInstrument->noteOff, pInstrument->note, 127);
-  midiMsg(pInstrument->noteOn, pInstrument->note, (float)velocity * pInstrument->velocityMultiplier);
+  velocity = (float)velocity * pInstrument->velocityMultiplier;
+    
+  midiMsg(pInstrument->noteOn, pInstrument->note, velocity);
+  pInstrument->isPlaying = true;
   
   resetInstrument(pInstrument);
   
